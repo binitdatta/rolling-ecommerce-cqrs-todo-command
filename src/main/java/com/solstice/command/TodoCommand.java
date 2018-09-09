1502ded188123fec4e68918986610274fb45bd7b
Binit Datta <binitdatta@gmail.com>
2018-09-09 07:01:53 -0500

    fixed

--- src/main/java/com/solstice/command/TodoCommand.java
+++ src/main/java/com/solstice/command/TodoCommand.java
@@ -1,46 +1,52 @@
 package com.solstice.command;
 
-@SuppressWarnings("unused")
+import java.util.UUID;
+
+import com.solstice.command.interfaces.GenericCommand;
+import com.solstice.domain.Todo;
+
 public class TodoCommand implements GenericCommand {
 
-    UUID id;
-    GenericCommandHeader header;
-    Todo todo;
+	UUID id;
+	GenericCommandHeader header;
+	Todo todo;
 
-    @Override
-    public GenericCommandHeader getHeader() {
-        return header;
-    }
+	@Override
+	public GenericCommandHeader getHeader() {
+		return header;
+	}
 
-    @Override
-    public void setHeader(GenericCommandHeader header) {
-        this.header = header;
-    }
+	@Override
+	public void setHeader(GenericCommandHeader header) {
+		this.header = header;
+	}
 
-    public UUID getId() {
-        return id;
-    }
+	public UUID getId() {
+		return id;
+	}
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
+	public void setId(UUID id) {
+		this.id = id;
+	}
 
-    public Todo getTodo() {
-        return todo;
-    }
+	public Todo getTodo() {
+		return todo;
+	}
 
-    public void setTodo(Todo todo) {
-        this.todo = todo;
-    }
+	public void setTodo(Todo todo) {
+		this.todo = todo;
+	}
 
-    public TodoCommand(UUID id, GenericCommandHeader header, Todo todo) {
-        super();
-        this.id = id;
-        this.header = header;
-        this.todo = todo;
-    }
+	public TodoCommand(UUID id, GenericCommandHeader header, Todo todo) {
+		super();
+		this.id = id;
+		this.header = header;
+		this.todo = todo;
+	}
 
-    public TodoCommand() {
-        super();
-    }
+	public TodoCommand() {
+		super();
+	}
+
+	
 }
