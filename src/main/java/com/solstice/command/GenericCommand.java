706a496adaa43b604890bb8e43210d9a076e5d1c
Binit Datta <binitdatta@gmail.com>
2018-09-09 07:00:21 -0500

    fixed

--- src/main/java/com/solstice/command/GenericCommand.java
+++ src/main/java/com/solstice/command/GenericCommand.java
@@ -1,5 +1,9 @@
 package com.solstice.command;
 
+import java.io.Serializable;
+
+import com.solstice.command.GenericCommandHeader;
+
 @SuppressWarnings("unused")
 public interface GenericCommand extends  Serializable {
     public GenericCommandHeader getHeader();
